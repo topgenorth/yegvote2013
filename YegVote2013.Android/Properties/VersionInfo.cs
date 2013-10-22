2ccb5e21d11f4ba8c1c043acf8cf86fbd552f418
Tom Opgenorth <tom@opgenorth.net>
2013-10-21 21:20:05 -0600

    Updating version numbers to 2.2013.0.17.

--- YegVote2013.Android/Properties/VersionInfo.cs
+++ YegVote2013.Android/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.2013.0.16")]
-[assembly: AssemblyFileVersion("2.2013.0.16")]
+[assembly: AssemblyVersion("2.2013.0.17")]
+[assembly: AssemblyFileVersion("2.2013.0.17")]
